d78e0bfb5a0093e32c3768872ee602b02c0de607
1449812719@qq.com <1449812719@qq.com>
2017-04-05 22:10:44 +0800

    吃辣条

--- src/com/hello/world/HelloWorld.java
+++ src/com/hello/world/HelloWorld.java
@@ -3,8 +3,14 @@ package com.hello.world;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		//System.out.println("HelloWorld");
+		small();
 
 	}
+	
+	public static void small(){
+		System.out.println("站住！辣条给我一根");
+	}
+
 
 }
